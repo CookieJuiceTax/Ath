6c6ee4d21545e1f9e27c36d242c73de0c586eceb
CookieJuiceTax <deepfriedoliveoil@gmail.com>
2025-05-22 14:28:30 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+function loadCSS(href) {
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = href;
+  document.head.appendChild(link);
+}
 
+loadCSS("https://cookiejuicetax.github.io/Ath/style.css");
+
+$(document).on(':storyready', function () {
+  $('#ui-bar').remove();
+  $(document.head).find('#style-ui-bar').remove();
+});
