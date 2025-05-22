985d0fc62eb58b49e033a75971c7faa46da4c47e
CookieJuiceTax <deepfriedoliveoil@gmail.com>
2025-05-22 21:12:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -9,3 +9,32 @@ loadCSS("https://cookiejuicetax.github.io/Poan/style.css");
 
 $('#ui-bar').remove();
 $(document.head).find('#style-ui-bar').remove();
+
+
+// Load Firebase SDK scripts
+function loadScript(src, callback) {
+  const script = document.createElement("script");
+  script.src = src;
+  script.onload = callback;
+  document.head.appendChild(script);
+}
+
+
+loadScript("https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js", function () {
+  loadScript("https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js", function () {
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBOGHmgo2Hc2j5f9rWMb1dYSNFuQWhcCuQ",
+  authDomain: "poan-57f54.firebaseapp.com",
+  projectId: "poan-57f54",
+  storageBucket: "poan-57f54.firebasestorage.app",
+  messagingSenderId: "860885646745",
+  appId: "1:860885646745:web:d5fcc385e3c85f78336708"
+};
+
+    firebase.initializeApp(firebaseConfig);
+    window.db = firebase.firestore();
+
+    console.log("✅ Firebase initialized");
+  });
+});
