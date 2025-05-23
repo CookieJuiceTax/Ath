fc93f3453ca28fe38fe30f9e6bb5c861f1f38715
CookieJuiceTax <deepfriedoliveoil@gmail.com>
2025-05-23 09:21:31 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 function loadCSS(href) {
   const link = document.createElement("link");
   link.rel = "stylesheet";
@@ -6,6 +7,7 @@ function loadCSS(href) {
 }
 loadCSS("https://cookiejuicetax.github.io/Poan/style.css");
 
+
 function loadScript(src, callback) {
   const script = document.createElement("script");
   script.src = src;
@@ -13,22 +15,26 @@ function loadScript(src, callback) {
   document.head.appendChild(script);
 }
 
+
 $(function () {
+
   $('#ui-bar').remove();
   $(document.head).find('#style-ui-bar').remove();
 
-loadScript("https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js", function () {
-  loadScript("https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore-compat.js", function () {
-    const firebaseConfig = {
-      apiKey: "AIzaSyBOGHmgo2Hc2j5f9rWMb1dYSNFuQWhcCuQ",
-      authDomain: "poan-57f54.firebaseapp.com",
-      projectId: "poan-57f54",
-      messagingSenderId: "860885646745",
-      appId: "1:860885646745:web:d5fcc385e3c85f78336708"
-    };
 
-    firebase.initializeApp(firebaseConfig);
-    window.db = firebase.firestore();
-    console.log("✅ Firebase initialized");
+  loadScript("https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js", function () {
+    loadScript("https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore-compat.js", function () {
+      const firebaseConfig = {
+        apiKey: "AIzaSyBOGHmgo2Hc2j5f9rWMb1dYSNFuQWhcCuQ",
+        authDomain: "poan-57f54.firebaseapp.com",
+        projectId: "poan-57f54",
+        messagingSenderId: "860885646745",
+        appId: "1:860885646745:web:d5fcc385e3c85f78336708"
+      };
+
+      firebase.initializeApp(firebaseConfig);
+      window.db = firebase.firestore();
+      console.log("✅ Firebase initialized");
+    });
   });
 });
